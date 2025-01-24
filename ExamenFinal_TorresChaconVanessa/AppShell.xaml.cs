2422f0ca529b14e8ef25faf4c89f66fd18956aee
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2025-01-24 08:57:45 -0500

    cambios appshell

--- ExamenFinal_TorresChaconVanessa/AppShell.xaml.cs
+++ ExamenFinal_TorresChaconVanessa/AppShell.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace ExamenFinal_TorresChaconVanessa
+﻿using ExamenFinal_TorresChaconVanessa.Views;
+namespace ExamenFinal_TorresChaconVanessa
+
+
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        public AppShell(SearchPage searchPage)
         {
             InitializeComponent();
+
+
+            Items.Add(new ShellContent
+            {
+                Title = "Buscar",
+                Content = searchPage
+            });
         }
+
     }
 }
+
+
+
