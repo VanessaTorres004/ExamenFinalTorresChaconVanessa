db8cc0e2f8798d82850d47838c926269ae9fc674
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2025-01-24 07:22:29 -0500

    Comit1

---
+++ ExamenFinal_TorresChaconVanessa/Models/Aeropuerto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal_TorresChaconVanessa.Models
+{
+    internal class Aeropuerto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Email { get; set; }
+        public string Vtorres { get; set; }
+    }
+}
